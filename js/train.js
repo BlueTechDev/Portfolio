2a5e8d360dd17edfc082445c6ab2c1a9b94f974d
Jonah Kroll <krolljl@mlc-wels.edu>
2023-09-20 11:02:18 -0400

    Update 1.2 #TrainCounter has been completed with additional styles and features

--- js/train.js
+++ js/train.js
@@ -11,7 +11,7 @@ const message = document.getElementById("saved-message");
 const resetPassengersElement = document.getElementById("resetbtn");
 const inputPassengerCount = document.querySelector("input[type='number']");
 
-let maxCapacity = 99;
+let maxCapacity = 100;
 
 // Function to update the current time
 function updateCurrentTime() {
@@ -22,13 +22,6 @@ function updateCurrentTime() {
     document.getElementById('current-minute').textContent = currentMinute;
     const currentSeconds = formatTimeWithLeadingZero(currentTime.getSeconds());
     document.getElementById("currentSeconds").textContent = currentSeconds;
-}
-
-// Function to update the current date
-
-// Function to format the time with leading zeros
-function formatTimeWithLeadingZero(time) {
-    return time.toString().padStart(2, '0');
 }
 
 // Function to update the current date
@@ -49,58 +42,110 @@ updateCurrentDate();
 // Update the current date every minute (60,000 milliseconds)
 setInterval(updateCurrentDate, 60000);
 
-
 // Call the updateCurrentTime function initially to set the current time on page load
 updateCurrentTime();
 
-// Update the current time every minute (60,000 milliseconds)
-setInterval(updateCurrentTime, 60000);
+// Update the current time every second (1000 milliseconds)
+setInterval(updateCurrentTime, 1000);
 
 // Function to increase passenger count
-
 let passengerCount = 0;
-let savedPassengers = 0;
 
 function increasePassengerCount() {
-    if (passengerCount > 14) {
+    if (passengerCount >= maxCapacity) {
         alert('Maximum number of passengers reached!');
-        return false; // prevent default form submission behavior when max limit is exceeded
+        return;
     }
-  passengerCount++;
-  console.log(`Passenger Count: ${passengerCount}`);
-  const passengerCountElement = document.getElementById("passenger-count");
-  passengerCountElement.textContent = passengerCount;
+    passengerCount++;
+    console.log(`Passenger Count: ${passengerCount}`);
+    const passengerCountElement = document.getElementById("passenger-count");
+    passengerCountElement.textContent = passengerCount;
+
+    // Additional animation: Increase size when passenger count is incremented
+    passengerCountElement.classList.add("animate-increase");
+    setTimeout(() => {
+        passengerCountElement.classList.remove("animate-increase");
+    }, 500);
 }
+
 increaseBtn.addEventListener("click", increasePassengerCount);
 
 function decreasePassengerCount() {
-    if (passengerCount < 1 ) {
+    if (passengerCount <= 0) {
         alert('Minimum number of passengers already met.');
-        return false;
+        return;
     }
     passengerCount--;
     console.log(`Passenger count: ${passengerCount}`);
     const passengerCountElement = document.getElementById("passenger-count");
     passengerCountElement.textContent = passengerCount;
+
+    // Additional animation: Decrease size when passenger count is decremented
+    passengerCountElement.classList.add("animate-decrease");
+    setTimeout(() => {
+        passengerCountElement.classList.remove("animate-decrease");
+    }, 500);
 }
+
 decreaseBtn.addEventListener("click", decreasePassengerCount);
 
 function savePassengers() {
-    savedPassengers = passengerCount;
-    console.log(`"Number of passengers boarded: ${passengerCount}`);
     const savedMessageElement = document.getElementById("saved-message");
-  savedMessageElement.querySelector("span").textContent = savedPassengers;
+    savedMessageElement.classList.add("animate-save");
+    setTimeout(() => {
+        savedMessageElement.classList.remove("animate-save");
+        savedMessageElement.textContent = `Number of passengers boarded: ${passengerCount}`;
+    }, 2000);
 }
+
 saveTrain.addEventListener("click", savePassengers);
 
 function resetPassengers() {
     passengerCount = 0;
-    savedPassengers = 0;
     alert("Passengers exiting the train stand back!!");
     console.log(`Resetting Passenger Count`);
     const passengerCountElement = document.getElementById("passenger-count");
     passengerCountElement.textContent = passengerCount;
     const savedMessageElement = document.getElementById("saved-message");
-    savedMessageElement.querySelector("span").textContent = savedPassengers;
-  }
-  resetPassengersElement.addEventListener("click", resetPassengers);
+    savedMessageElement.textContent = "";
+}
+
+resetPassengersElement.addEventListener("click", resetPassengers);
+
+const customInput = document.getElementById("custom-passenger-count");
+const customSubmit = document.getElementById("custom-submit");
+
+customInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Prevent the form from being submitted
+        const customCount = parseInt(customInput.value);
+        if (isNaN(customCount)) {
+            alert("Invalid input. Please enter a valid number.");
+            return;
+        }
+        if (customCount < 0 || customCount > maxCapacity) {
+            alert("Invalid input. Please enter a number within the valid range.");
+            return;
+        }
+        passengerCount = customCount;
+        const passengerCountElement = document.getElementById("passenger-count");
+        passengerCountElement.textContent = passengerCount;
+        customInput.value = ""; // Clear the input field
+    }
+});
+
+customSubmit.addEventListener("click", function() {
+    const customCount = parseInt(customInput.value);
+    if (isNaN(customCount)) {
+        alert("Invalid input. Please enter a valid number.");
+        return;
+    }
+    if (customCount < 0 || customCount > maxCapacity) {
+        alert("Invalid input. Please enter a number within the valid range.");
+        return;
+    }
+    passengerCount = customCount;
+    const passengerCountElement = document.getElementById("passenger-count");
+    passengerCountElement.textContent = passengerCount;
+    customInput.value = ""; // Clear the input field
+});
